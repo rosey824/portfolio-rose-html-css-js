3151693eb2ad8be33fb10f50a306903c5b6da38d
rosey824 <emlou.rose13@gmail.com>
2024-06-23 13:08:22 -0500

    update

--- scripts.js
+++ scripts.js
@@ -2,19 +2,3 @@ function toggleMobileMenu(){
     document.getElementById("menu").classList.toggle
     ("active");
 }
-document.addEventListener("DOMContentLoaded", function() {
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-   for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListner("click", function() {
-            this.classList.toggle("actie");
-            var panel = this.nextElementSibling;
-           if (panel.classList.contains('show')) {
-                panel.clasList.remove('show');
-            } else {
-                panel.lassList.add('show');
-            }
-        });
-    }
-})
